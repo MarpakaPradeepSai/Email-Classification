545e0e1f2a22bbed7ab692ba3da35057cf6f215c
Marpaka Pradeep Sai <153713502+MarpakaPradeepSai@users.noreply.github.com>
2024-09-18 12:33:04 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,8 +3,8 @@ import joblib
 import pandas as pd
 
 # Load the saved model and vectorizer
-model = joblib.load('logistic_regression_CV(0.985650).pkl')
-vectorizer = joblib.load('complete_cv_vectorizer.pkl')
+model = joblib.load('bernoulli_NB_TFIDF(0.991928).pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 # Set up the Streamlit app
 st.title("Spam/Ham Classifier")
